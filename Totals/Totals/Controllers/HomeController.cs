f51a5f7a27367a3880eacc04b241ad9e8bb3e929
Liviu Mandras <liviu@mandras-iura.ro>
2020-09-19 19:49:19 +0300

    changed text 2

--- Totals/Totals/Controllers/HomeController.cs
+++ Totals/Totals/Controllers/HomeController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
-using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
