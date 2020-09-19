a9c3e833e2d36e3c120708424d959d1af217a23f
Liviu Mandras <liviu@mandras-iura.ro>
2020-09-19 09:34:53 +0300

    added file upload

--- Totals/Totals/Controllers/HomeController.cs
+++ Totals/Totals/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Totals.Models;
@@ -22,10 +24,29 @@ namespace Totals.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Upload(List<IFormFile> files)
         {
-            return View();
+            long size = files.Sum(f => f.Length);
+                        
+            var filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    // full path to file in temp location
+                    var filePath = Path.GetTempFileName(); //we are using Temp file name just for the example. Add your own file path.
+                    filePaths.Add(filePath);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+            return Ok(new { count = files.Count, size, filePaths });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
